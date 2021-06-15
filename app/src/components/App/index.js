f5f9cc12b0380235d56f4d85f5724220cb0807cc
Adrian <adrianalexander17@gmail.com>
2021-06-15 12:36:06 -0400

    commented out not used components yet

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Navigation from "../Navigation";
-import LandingPage from "../Landing";
-import RegisterPage from "../Register";
-import LoginPage from "../Login";
-import HomePage from "../Home";
-import AccountPage from "../Account";
+// import LandingPage from "../Landing";
+// import RegisterPage from "../Register";
+// import LoginPage from "../Login";
+// import HomePage from "../Home";
+// import AccountPage from "../Account";
 
 import * as ROUTES from "../../constants/routes"
 
@@ -16,11 +16,11 @@ export default function App() {
       <div>
         <Navigation />
 
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
         <Route path={ROUTES.REGISTER} component={RegisterPage} />
         <Route path={ROUTES.LOGIN} component={LoginPage} />
         <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
       </div>
     </Router>
   );
